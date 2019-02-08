ebdd049c51350df0974a4007f521d842d2b01ca1
nilabja9 <nilabjaghohchowdhury@gmail.com>
2019-02-08 08:43:35 -0600

    Script Added to Grab Code from Twitter

---
+++ TwitterGrabData.py
@@ -0,0 +1,36 @@
+import tweepy
+from tweepy import OAuthHandler
+import boto3
+import json
+import time
+import twittercred
+import awscred
+
+auth = OAuthHandler(twittercred.consumerkey, twittercred.consumersecret)
+auth.set_access_token(twittercred.accesskey, twittercred.accesssecret)
+
+api = tweepy.API(auth)
+topic = 'Donald Trump'
+
+if __name__ == '__main__':
+
+    client = boto3.client('firehose', aws_access_key_id = awscred.accesskey, aws_secret_access_key = awscred.accesssecret)
+
+    while True:
+        for tweet in tweepy.Cursor(api.search, q=topic).items(1000):
+            response = client.put_record(
+                DeliveryStreamName='twitterkinesis',
+                Record={
+                    'Data': json.dumps(tweet._json) + '\n'
+                }
+
+
+            )
+            print(response)
+
+
+
+
+        time.sleep(300)
+
+
