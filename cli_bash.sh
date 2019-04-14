4a6891c5cfa11be7a3f69cdc44b0b8354a92a54b
Nilabja GhoshChowdhury <nilabjaghoshchowdhury@gmail.com>
2019-04-13 23:16:14 -0500

    Update cli_bash.sh

--- cli_bash.sh
+++ cli_bash.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-aws configure set aws_access_key_id AKIAJKSMK7MXZJ3BNKVQ
-aws configure set aws_secret_access_key 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
+aws configure set aws_access_key_id XXXXXXXXXXXXXXXXXX
+aws configure set aws_secret_access_key XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 aws configure set default.region us-east-1
 aws emr create-cluster --name "MySparkTestCluster" --release-label emr-5.23.0 --applications Name=Spark --instance-type m4.large --instance-count 3 --steps Type=CUSTOM_JAR,Name="first",Jar="command-runner.jar",ActionOnFailure=CONTINUE,Args=["spark-submit","--deploy-mode","cluster","s3://nilabjascripts/sparkprocess_v2.py"] --use-default-roles --auto-terminate
